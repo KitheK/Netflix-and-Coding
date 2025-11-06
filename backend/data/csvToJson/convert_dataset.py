481ebfd76eac0ef3512c18b2aabb6d7e291a7297
frasermuller <frasermuller2005@gmail.com>
2025-11-05 20:14:22 -0800

    created file to change amazon.csv to products.json, products are in json format and much easier to read

---
+++ backend/data/csvToJson/convert_dataset.py
@@ -0,0 +1,13 @@
+##convert amazon.csv to .json for easier use. because FastAPI returns and recieves json.
+#amazon.csv to products.json
+
+import pandas as pd
+from pathlib import Path
+
+csv_path = Path("backend/data/amazon.csv")
+json_path = Path("backend/data/products.json")
+
+df = pd.read_csv(csv_path)
+df.to_json(json_path, orient="records", indent=4) 
+
+print(f"converted {csv_path} to {json_path} successfully.")
